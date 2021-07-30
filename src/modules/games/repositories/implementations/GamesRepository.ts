6165cd3eb23972282bdd1281e68ea91164140ee2
Recieire Carbelim <recieiremarcos@gmail.com>
2021-07-30 00:58:14 -0300

    done

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -16,15 +16,20 @@ export class GamesRepository implements IGamesRepository {
     return this.repository
       .createQueryBuilder()
       // Complete usando query builder
+      .where('title ILIKE :title', { title: `%${param}%` })
+      .getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT COUNT(id) FROM games'); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
       .createQueryBuilder()
       // Complete usando query builder
+      .relation(Game, "users")
+      .of(id)
+      .loadMany();
   }
 }
